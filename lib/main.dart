d9f4594a1e16471745fe1985d3b18974f9eec6cb
kth5352 <kth5352@gmail.com>
2023-11-07 05:08:20 +0900

    new images, fixed pubspec.yaml, add btn from main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: library_private_types_in_public_api
 
 import 'package:flutter/material.dart';
+import 'package:flutter_application/Add_Food.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,8 +14,7 @@ class MyApp extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<MyApp>
-    with SingleTickerProviderStateMixin {
+class _HomeScreenState extends State<MyApp> with SingleTickerProviderStateMixin {
   late TabController controller;
 
   @override
@@ -28,40 +28,54 @@ class _HomeScreenState extends State<MyApp>
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         home: Scaffold(
-            appBar: AppBar(
-              title: const Text('AppName'),
-              bottom: TabBar(
-                controller: controller,
-                tabs: const <Widget>[
-                  Tab(text: '냉장고'),
-                  Tab(text: '냉동실'),
-                  Tab(text: '실온'),
-                ],
-              ),
-            ),
-            //add........................
-            body: TabBarView(
+          appBar: AppBar(
+            title: const Text('AppName'),
+            bottom: TabBar(
               controller: controller,
-              children: const <Widget>[
-                Center(
-                  child: Text(
-                    'One Screen',
-                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-                  ),
-                ),
-                Center(
-                  child: Text(
-                    'Two Screen',
-                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-                  ),
-                ),
-                Center(
-                  child: Text(
-                    'Three Screen',
-                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-                  ),
-                )
+              tabs: const <Widget>[
+                Tab(text: '냉장고'),
+                Tab(text: '냉동실'),
+                Tab(text: '실온'),
               ],
-            )));
+            ),
+          ),
+          //add........................
+          body: TabBarView(
+            controller: controller,
+            children: const <Widget>[
+              Center(
+                child: Text(
+                  'One Screen',
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Center(
+                child: Text(
+                  'Two Screen',
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Center(
+                child: Text(
+                  'Three Screen',
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                ),
+              )
+            ],
+          ),
+          floatingActionButton: Builder(
+            builder: (context) => FloatingActionButton(
+              onPressed: () {
+                // AddFoodScreen으로 화면 전환
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const AddFoodScreen()),
+                );
+              },
+              child: const Icon(Icons.add), // '+' 아이콘
+            ),
+          ),
+          floatingActionButtonLocation: FloatingActionButtonLocation.endFloat, // 우측 하단 위치
+        ));
   }
 }
