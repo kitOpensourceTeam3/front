650a87ef7bea1ae8571fee25b7434020e5416c5d
ZeppIine <kim0904min@gmail.com>
2023-11-07 19:27:10 +0900

    add foodlist page

--- lib/food_list.dart
+++ lib/food_list.dart
@@ -1,1 +1,20 @@
+import 'package:flutter/material.dart';
 
+class FoodListScreen extends StatelessWidget {
+  const FoodListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+      title: const Text(
+        '음식 추가',
+        style: TextStyle(color: Colors.black), // 여기에서 글자 색상을 지정합니다.
+      ),
+      backgroundColor: Colors.white, // AppBar 배경색을 지정합니다.
+      iconTheme: const IconThemeData(
+        color: Colors.black, // 뒤로 가기 버튼의 색상을 검은색으로 지정합니다.
+      ),
+    ));
+  }
+}
