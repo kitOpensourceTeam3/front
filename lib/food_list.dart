966031c8ac130a455b5fcfcd0bdc8859c8be4bf5
ZeppIine <kim0904min@gmail.com>
2023-11-07 20:25:05 +0900

    add buttons

--- lib/food_list.dart
+++ lib/food_list.dart
@@ -6,15 +6,51 @@ class FoodListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      title: const Text(
-        '음식 추가',
-        style: TextStyle(color: Colors.black), // 여기에서 글자 색상을 지정합니다.
+      appBar: AppBar(
+        title: const Text(
+          '음식 추가',
+          style: TextStyle(color: Colors.black),
+        ),
+        backgroundColor: Colors.white,
+        iconTheme: const IconThemeData(
+          color: Colors.black,
+        ),
       ),
-      backgroundColor: Colors.white, // AppBar 배경색을 지정합니다.
-      iconTheme: const IconThemeData(
-        color: Colors.black, // 뒤로 가기 버튼의 색상을 검은색으로 지정합니다.
+      body: Column(
+        children: [
+          Expanded(
+            child: Center(
+              child: buildMainMenu(),
+            ),
+          ),
+        ],
       ),
-    ));
+    );
+  }
+
+  Widget buildMainMenu() {
+    return GridView.builder(
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 4,
+        childAspectRatio: 1.0,
+      ),
+      itemCount: 8,
+      itemBuilder: (BuildContext context, int index) {
+        return InkWell(
+            onTap: () {
+              // Handle button tap here
+            },
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20), // 버튼 라운드 하드처리
+              child: Container(
+                margin: const EdgeInsets.all(8),
+                color: Colors.blue,
+                child: Center(
+                  child: Text('식품 버튼 ${index + 1}'),
+                ),
+              ),
+            ));
+      },
+    );
   }
 }
