460e32f2d4462dee8cd60b45333584ac01c34c8b
ZeppIine <kim0904min@gmail.com>
2023-11-07 18:58:33 +0900

    add food_list.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: library_private_types_in_public_api
 
 import 'package:flutter/material.dart';
+import 'package:flutter_application/food_list.dart';
 
 void main() {
   runApp(const MyApp());
