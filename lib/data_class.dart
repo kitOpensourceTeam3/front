4196f767ad4982959234cc97cbd971d32bac9a70
ZeppIine <kim0904min@gmail.com>
2023-12-03 20:54:45 +0900

    rollback lowercamelcase

--- lib/data_class.dart
+++ lib/data_class.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class FoodData {
   int imagePath;
   int quantity;
-  int expdate;
+  int exp_date;
   String namePath;
   String selectedStorage;
   DateTime selectedDate;
@@ -13,13 +13,13 @@ class FoodData {
   FoodData({
     required this.imagePath,
     this.quantity = 1,
-    required this.expdate,
+    required this.exp_date,
     required this.namePath,
     this.selectedStorage = '냉장고',
     DateTime? selectedDate,
     DateTime? expirationDate,
     TextEditingController? noteController,
   })  : selectedDate = selectedDate ?? DateTime.now(),
-        expirationDate = expirationDate ?? DateTime.now().add(Duration(days: expdate)),
+        expirationDate = expirationDate ?? DateTime.now().add(Duration(days: exp_date)),
         noteController = noteController ?? TextEditingController();
 }
