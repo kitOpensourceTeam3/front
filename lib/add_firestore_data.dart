51c1372744ff39109ff2508b9bcc630804070665
ZeppIine <kim0904min@gmail.com>
2023-12-04 01:14:09 +0900

    데이터 추가 기능 구?현

--- lib/add_firestore_data.dart
+++ lib/add_firestore_data.dart
@@ -1,26 +1,38 @@
-import 'dart:convert';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application/data_class.dart';
 
-// class AddFirestoreData extends StatelessWidget {
-//   final int foodId;
-//   final FoodData foodData;
+class AddFirestoreData extends StatelessWidget {
+  final int foodId;
+  final FoodData foodData;
 
-//   const AddFirestoreData({super.key, required this.foodId, required this.foodData});
+  const AddFirestoreData({super.key, required this.foodId, required this.foodData});
 
-//   @override
-//   Future<Widget> build(BuildContext context) async {
-//     Future createNewMotto(Map<String, dynamic> json) async {
-//       DocumentReference<Map<String, dynamic>> documentReference =
-//           FirebaseFirestore.instance.collection("motto_db").doc();
-//       final DocumentSnapshot<Map<String, dynamic>> documentSnapshot = await documentReference.get();
+  @override
+  Widget build(BuildContext context) {
+    Future<void> createNewFoodData(String uid) async {
+      Map<String, dynamic> jsonData = {
+        'uid': uid,
+        'f_id': foodId,
+        'quantity': foodData.quantity,
+        'add_day': Timestamp.fromDate(foodData.selectedDate),
+        'exp_day': Timestamp.fromDate(foodData.expirationDate),
+        'type': foodData.selectedStorage,
+        'memo': foodData.noteController.text,
+      };
 
-//       if (!documentSnapshot.exists) {
-//         await documentReference.set(json);
-//       }
-//     }
-//       return createNewMotto(json);
-//   }
-// }
+      CollectionReference foodDataCollection = FirebaseFirestore.instance.collection("food_data");
+      await foodDataCollection.add(jsonData);
+    }
+
+    String? uid = FirebaseAuth.instance.currentUser?.uid;
+
+    if (uid != null) {
+      createNewFoodData(uid);
+      Navigator.pop(context);
+    }
+
+    return Container();
+  }
+}
--- lib/add_food_data.dart
+++ lib/add_food_data.dart
@@ -84,11 +84,7 @@ class _AddFoodDataState extends State<AddFoodData> {
               if (imageData.isNotEmpty) {
                 String imagePath = imageData.first['f_name'] as String;
 
-                return Image.network(
-                  imagePath,
-                  width: 100,
-                  fit: BoxFit.cover,
-                );
+                return Image.network(imagePath, height: 90, width: 100);
               } else {
                 return const Text("No data found");
               }
@@ -255,7 +251,10 @@ class _AddFoodDataState extends State<AddFoodData> {
       right: 50,
       child: ElevatedButton(
         onPressed: () {
-          // AddFirestoreData(foodId: foodId, foodData: foodData);
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => AddFirestoreData(foodId: foodId, foodData: foodData)));
         },
         style: ElevatedButton.styleFrom(
           backgroundColor: Colors.blue,
